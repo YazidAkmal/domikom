82698840af83455429ccf93624b408c401ea038d
Yazid Akmal Adyatma <yazidakmal1234@gmail.com>
2024-12-02 21:54:24 +0700

    first commit

---
+++ uts/finalProject/finalProject/dashboard.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace finalProject
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnHome = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDonation = new Guna.UI2.WinForms.Guna2Button();
+            this.btnRequest = new Guna.UI2.WinForms.Guna2Button();
+            this.btnHistory = new Guna.UI2.WinForms.Guna2Button();
+            this.btnProfile = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.guna2Panel1.Controls.Add(this.btnProfile);
+            this.guna2Panel1.Controls.Add(this.btnRequest);
+            this.guna2Panel1.Controls.Add(this.btnHistory);
+            this.guna2Panel1.Controls.Add(this.btnDonation);
+            this.guna2Panel1.Controls.Add(this.btnHome);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(215, 749);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // btnHome
+            // 
+            this.btnHome.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.btnHome.CustomImages.CheckedImage = global::finalProject.Properties.Resources.home;
+            this.btnHome.CustomImages.HoveredImage = global::finalProject.Properties.Resources.home__2_;
+            this.btnHome.CustomImages.Image = global::finalProject.Properties.Resources.home;
+            this.btnHome.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnHome.CustomImages.ImageOffset = new System.Drawing.Point(30, 0);
+            this.btnHome.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnHome.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnHome.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnHome.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnHome.FillColor = System.Drawing.Color.White;
+            this.btnHome.Font = new System.Drawing.Font("Montserrat SemiBold", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.btnHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnHome.HoverState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnHome.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnHome.ImageOffset = new System.Drawing.Point(70, 0);
+            this.btnHome.Location = new System.Drawing.Point(0, 164);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(215, 40);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextOffset = new System.Drawing.Point(6, 0);
+            // 
+            // btnDonation
+            // 
+            this.btnDonation.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.btnDonation.CustomImages.CheckedImage = global::finalProject.Properties.Resources.money;
+            this.btnDonation.CustomImages.HoveredImage = global::finalProject.Properties.Resources.money__1_;
+            this.btnDonation.CustomImages.Image = global::finalProject.Properties.Resources.money;
+            this.btnDonation.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnDonation.CustomImages.ImageOffset = new System.Drawing.Point(30, 0);
+            this.btnDonation.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDonation.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDonation.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDonation.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDonation.FillColor = System.Drawing.Color.White;
+            this.btnDonation.Font = new System.Drawing.Font("Montserrat SemiBold", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.btnDonation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnDonation.HoverState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnDonation.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnDonation.ImageOffset = new System.Drawing.Point(70, 0);
+            this.btnDonation.Location = new System.Drawing.Point(0, 207);
+            this.btnDonation.Name = "btnDonation";
+            this.btnDonation.Size = new System.Drawing.Size(215, 40);
+            this.btnDonation.TabIndex = 2;
+            this.btnDonation.Text = "Donation";
+            this.btnDonation.TextOffset = new System.Drawing.Point(6, 0);
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.btnRequest.CustomImages.CheckedImage = global::finalProject.Properties.Resources.request_for_proposal;
+            this.btnRequest.CustomImages.HoveredImage = global::finalProject.Properties.Resources.request_for_proposal__1_;
+            this.btnRequest.CustomImages.Image = global::finalProject.Properties.Resources.request_for_proposal;
+            this.btnRequest.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnRequest.CustomImages.ImageOffset = new System.Drawing.Point(30, 0);
+            this.btnRequest.CustomImages.ImageSize = new System.Drawing.Size(25, 25);
+            this.btnRequest.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnRequest.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnRequest.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnRequest.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnRequest.FillColor = System.Drawing.Color.White;
+            this.btnRequest.Font = new System.Drawing.Font("Montserrat SemiBold", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.btnRequest.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnRequest.HoverState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnRequest.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnRequest.ImageOffset = new System.Drawing.Point(70, 0);
+            this.btnRequest.Location = new System.Drawing.Point(0, 294);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(215, 40);
+            this.btnRequest.TabIndex = 4;
+            this.btnRequest.Text = "Request";
+            this.btnRequest.TextOffset = new System.Drawing.Point(6, 0);
+            // 
+            // btnHistory
+            // 
+            this.btnHistory.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.btnHistory.CustomImages.CheckedImage = global::finalProject.Properties.Resources.history__2_;
+            this.btnHistory.CustomImages.HoveredImage = global::finalProject.Properties.Resources.history__3_;
+            this.btnHistory.CustomImages.Image = global::finalProject.Properties.Resources.history__2_;
+            this.btnHistory.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnHistory.CustomImages.ImageOffset = new System.Drawing.Point(30, 0);
+            this.btnHistory.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnHistory.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnHistory.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnHistory.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnHistory.FillColor = System.Drawing.Color.White;
+            this.btnHistory.Font = new System.Drawing.Font("Montserrat SemiBold", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.btnHistory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnHistory.HoverState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnHistory.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnHistory.ImageOffset = new System.Drawing.Point(70, 0);
+            this.btnHistory.Location = new System.Drawing.Point(0, 251);
+            this.btnHistory.Name = "btnHistory";
+            this.btnHistory.Size = new System.Drawing.Size(215, 40);
+            this.btnHistory.TabIndex = 3;
+            this.btnHistory.Text = "History";
+            this.btnHistory.TextOffset = new System.Drawing.Point(6, 0);
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.btnProfile.CustomImages.CheckedImage = global::finalProject.Properties.Resources.user__3_;
+            this.btnProfile.CustomImages.HoveredImage = global::finalProject.Properties.Resources.user__2_;
+            this.btnProfile.CustomImages.Image = global::finalProject.Properties.Resources.user__3_;
+            this.btnProfile.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnProfile.CustomImages.ImageOffset = new System.Drawing.Point(33, 0);
+            this.btnProfile.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnProfile.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnProfile.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnProfile.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnProfile.FillColor = System.Drawing.Color.White;
+            this.btnProfile.Font = new System.Drawing.Font("Montserrat SemiBold", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.btnProfile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnProfile.HoverState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(38)))), ((int)(((byte)(137)))));
+            this.btnProfile.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnProfile.ImageOffset = new System.Drawing.Point(70, 0);
+            this.btnProfile.Location = new System.Drawing.Point(0, 338);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.Size = new System.Drawing.Size(215, 40);
+            this.btnProfile.TabIndex = 5;
+            this.btnProfile.Text = "Profile";
+            this.btnProfile.TextOffset = new System.Drawing.Point(6, 0);
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::finalProject.Properties.Resources.PANJANG;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(46, 64);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(128, 42);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 749);
+            this.Controls.Add(this.guna2Panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DOMIKOM Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnHome;
+        private Guna.UI2.WinForms.Guna2Button btnDonation;
+        private Guna.UI2.WinForms.Guna2Button btnRequest;
+        private Guna.UI2.WinForms.Guna2Button btnHistory;
+        private Guna.UI2.WinForms.Guna2Button btnProfile;
+    }
+}
